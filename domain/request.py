546fed890fefef37a9aebd2e79c8401309e85237
Nik <zhmakinkolay@mail.ru>
2022-02-07 16:54:02 +0500

    третья задача

--- domain/request.py
+++ domain/request.py
@@ -1,4 +1,10 @@
+import math
+
+
 class Request:
+    LAT_STEP = 0.008
+    LON_STEP = 0.02
+
     def __init__(self):
         self.longitude = 37.530887
         self.latitude = 55.703118
@@ -18,9 +24,21 @@ class Request:
         return self.type
 
     def zoom_up(self):
-        if self.zoom < 17:
+        if self.zoom < 15:
             self.zoom += 1
 
     def zoom_down(self):
         if self.zoom > 0:
             self.zoom -= 1
+
+    def left(self):
+        self.longitude -= self.LON_STEP * math.pow(2, 15 - self.zoom)
+
+    def right(self):
+        self.longitude += self.LON_STEP * math.pow(2, 15 - self.zoom)
+
+    def up(self):
+        self.latitude += self.LAT_STEP * math.pow(2, 15 - self.zoom)
+
+    def down(self):
+        self.latitude -= self.LAT_STEP * math.pow(2, 15 - self.zoom)
